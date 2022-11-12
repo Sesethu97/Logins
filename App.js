21f4ef231ceb19c7fae24ec769faf7e2624a795a
sluzipo021 <sluzipo021@student.wethinkcode.co.za>
2022-11-12 21:32:44 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+
+import React from 'react';
+import {
+  SafeAreaView,Text
+  
+} from 'react-native';
+import Register from './src/screens/Registeration';
+
+const App = ()  => {
+
+  return (
+    <SafeAreaView>
+      <Text>
+        <Register/>
+      </Text>
+
+    </SafeAreaView>
+  );
+};
+
+export default App;
